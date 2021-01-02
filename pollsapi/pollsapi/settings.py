62b1e74b5c7d1f466d815452705ce166166d80e3
Gabriel Lee <gabrielleeyj@outlook.com>
2021-01-02 23:02:38 +0800

    removed keys

--- pollsapi/pollsapi/settings.py
+++ pollsapi/pollsapi/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*2_)5wtfj5e9py!fu%lafu%36lqad7zmztatc+l!0!c)ex*nv8'
+SECRET_KEY = os.environ.get('SECRECT')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
